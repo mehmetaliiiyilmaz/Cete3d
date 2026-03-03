0cbf13db0cb6d60a34e00deafbe7c27064774909
mehmetaliiiyilmaz <mehmetaliiiyilmaz41.28@gmail.com>
2026-03-03 11:59:34 +0300

    Update products.js

--- products.js
+++ products.js
@@ -1,20 +1,20 @@
 const products = [
-{
-    name: "Özel Anahtarlık",
-    category: "anahtarlik",
-    price: 50,
-    image: "https://via.placeholder.com/300x200",
-},
-{
-    name: "3D Figür",
-    category: "figur",
-    price: 150,
-    image: "https://via.placeholder.com/300x200",
-},
-{
-    name: "Dekoratif Obje",
-    category: "dekor",
-    price: 200,
-    image: "https://via.placeholder.com/300x200",
-}
+    {
+        name: "Özel Anahtarlık",
+        category: "anahtarlik",
+        price: 50,
+        image: "https://via.placeholder.com/300x200",
+    },
+    {
+        name: "3D Figür",
+        category: "figur",
+        price: 150,
+        image: "https://via.placeholder.com/300x200",
+    },
+    {
+        name: "Dekoratif Obje",
+        category: "dekor",
+        price: 200,
+        image: "https://via.placeholder.com/300x200",
+    }
 ];
